628b681c0862571fa07aa0fc35ff38821e7fd2e9
sxv110 <sxv2480@gmail.com>
2025-03-20 16:00:56 -0500

    sure

--- src/driverPortal.js
+++ src/driverPortal.js
@@ -3,13 +3,6 @@ import MenuBar from './MenuBar';
 import ListingDriver from './listingDriver';
 import './driverPortal.css';
 import config from './CONSTANTS.js';
-import { getFirestore, doc, setDoc } from 'firebase/firestore';
-import { getAuth, initializeApp } from 'firebase/app';
-import firebaseConfig from './firebaseConfig'; // Import your Firebase configuration
-
-// Initialize Firebase app
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
 const DriverPortal = () => {
   const [showPopup, setShowPopup] = useState(false);
@@ -25,7 +18,7 @@ const DriverPortal = () => {
     setNewListing((prev) => ({ ...prev, [name]: value }));
   };
 
-  const handleNewListingSubmit = async () => {
+  const handleNewListingSubmit = () => {
     if (newListing.name && newListing.deliveryDate && newListing.destination) {
       const newEntry = {
         name: newListing.name,
@@ -35,15 +28,6 @@ const DriverPortal = () => {
         deliveryDate: newListing.deliveryDate,
         destination: newListing.destination,
       };
-      const docRef = doc(db, "drivers");
-            await setDoc(doc(docRef, newEntry.id.toString()), {
-              name: newListing.name,
-              address: newListing.address,
-              number: newListing.phoneNumber,
-              id: newEntry.id,
-              deliveryDate: newListing.deliveryDate,
-              destination: newListing.destination
-            });
       config.driverInfo.push(newEntry);
       console.error(config.driverInfo);
       setListings((prevListings) => [...prevListings, newEntry]);
@@ -67,6 +51,9 @@ const DriverPortal = () => {
         <h1 className='push listings-empty-error'>
           No Ride Scheduled Currently
         </h1>
+      ) : (
+        <>
+          <div className='push'>
             <span className='date list-header'>Delivery Date</span>
             <span className='location list-header'>Location</span>
           </div>
@@ -161,3 +148,4 @@ const DriverPortal = () => {
 };
 
 export default DriverPortal;
+/*asdasdasdasd*/
